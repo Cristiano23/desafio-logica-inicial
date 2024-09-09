563a4459f54b679e4788a2594adef0f3614b9003
Cristiano Santos <cristyano-crys@hotmail.com>
2024-09-09 15:31:40 -0300

    Atualização do projeto

--- lab-project-1/index.js
+++ lab-project-1/index.js
@@ -1,1 +1,28 @@
-console.log('Potência Tech iFood - Programação do Zero');
+const heroi = 'Max Pine';
+
+let xp = 2500;
+
+if (xp < 1000) {
+    console.log(heroi +  'Seu nível é ' + 'ferro');
+} 
+else if (xp >= 1001 && xp < 2000) {
+    console.log(heroi + 'Seu nível é ' + 'Bronze');
+}
+else if (xp >= 2001 && xp < 5000) {
+    console.log(heroi + 'Seu nível é ' + 'Prata');
+}
+else if (xp >= 6001 && xp < 6000) {
+    console.log(heroi + 'Seu nível é ' + 'Ouro');
+}
+else if (xp >= 5001 && xp < 8000) {
+    console.log(heroi + 'Seu nível é ' + 'Platina');
+}
+else if (xp >= 8001 && xp < 9000) {
+    console.log('Seu nível é ' + 'Ascendente');
+}
+else if (xp >= 9001 && xp < 10000) {
+    console.log('Seu nível é ' + 'Imortal');
+}
+else (xp >= 10.001) {
+    console.log('Seu nível é ' + 'Radiante');
+}
